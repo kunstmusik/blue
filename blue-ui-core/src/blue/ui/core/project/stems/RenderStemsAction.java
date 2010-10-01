ab488ff8373ff74dc3ab0d3868f12442d321657e
Steven Yi <stevenyi@gmail.com>
2010-10-01 18:54:09 -0400

    initial work on render stems wizard

---
+++ blue-ui-core/src/blue/ui/core/project/stems/RenderStemsAction.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blue.ui.core.project.stems;
+
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.DialogDisplayer;
+import org.openide.WizardDescriptor;
+
+public final class RenderStemsAction implements ActionListener {
+
+    public void actionPerformed(ActionEvent e) {
+        WizardDescriptor wd = new WizardDescriptor(new RenderStemsWizardIterator());
+
+        Dialog d = DialogDisplayer.getDefault().createDialog(wd);
+        d.setVisible(true);
+    }
+}
---
+++ blue-ui-core/src/blue/ui/core/project/stems/RenderStemsWizardIterator.java
@@ -0,0 +1,151 @@
+/*
+ * blue - object composition environment for csound
+ * Copyright (c) 2010 Steven Yi (stevenyi@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by  the Free Software Foundation; either version 2 of the License or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307 USA
+ */
+
+package blue.ui.core.project.stems;
+
+import java.awt.Component;
+import java.util.NoSuchElementException;
+import javax.swing.JComponent;
+import javax.swing.event.ChangeListener;
+import org.openide.WizardDescriptor;
+
+public final class RenderStemsWizardIterator implements WizardDescriptor.Iterator {
+
+    // To invoke this wizard, copy-paste and run the following code, e.g. from
+    // SomeAction.performAction():
+    /*
+    WizardDescriptor.Iterator iterator = new RenderStemsWizardIterator();
+    WizardDescriptor wizardDescriptor = new WizardDescriptor(iterator);
+    // {0} will be replaced by WizardDescriptor.Panel.getComponent().getName()
+    // {1} will be replaced by WizardDescriptor.Iterator.name()
+    wizardDescriptor.setTitleFormat(new MessageFormat("{0} ({1})"));
+    wizardDescriptor.setTitle("Your wizard dialog title here");
+    Dialog dialog = DialogDisplayer.getDefault().createDialog(wizardDescriptor);
+    dialog.setVisible(true);
+    dialog.toFront();
+    boolean cancelled = wizardDescriptor.getValue() != WizardDescriptor.FINISH_OPTION;
+    if (!cancelled) {
+    // do something
+    }
+     */
+    private int index;
+
+    private WizardDescriptor.Panel[] panels;
+
+    /**
+     * Initialize panels representing individual wizard's steps and sets
+     * various properties for them influencing wizard appearance.
+     */
+    private WizardDescriptor.Panel[] getPanels() {
+        if (panels == null) {
+            panels = new WizardDescriptor.Panel[]{
+                        new RenderStemsWizardPanel1(),
+                        new RenderStemsWizardPanel2(),
+                        new RenderStemsWizardPanel3(),
+                        new RenderStemsWizardPanel4(),
+                        new RenderStemsWizardPanel5()
+                    };
+            String[] steps = new String[panels.length];
+            for (int i = 0; i < panels.length; i++) {
+                Component c = panels[i].getComponent();
+                // Default step name to component name of panel.
+                steps[i] = c.getName();
+                if (c instanceof JComponent) { // assume Swing components
+                    JComponent jc = (JComponent) c;
+                    // Sets step number of a component
+                    // TODO if using org.openide.dialogs >= 7.8, can use WizardDescriptor.PROP_*:
+                    jc.putClientProperty("WizardPanel_contentSelectedIndex", new Integer(i));
+                    // Sets steps names for a panel
+                    jc.putClientProperty("WizardPanel_contentData", steps);
+                    // Turn on subtitle creation on each step
+                    jc.putClientProperty("WizardPanel_autoWizardStyle", Boolean.TRUE);
+                    // Show steps on the left side with the image on the background
+                    jc.putClientProperty("WizardPanel_contentDisplayed", Boolean.TRUE);
+                    // Turn on numbering of all steps
+                    jc.putClientProperty("WizardPanel_contentNumbered", Boolean.TRUE);
+                }
+            }
+        }
+        return panels;
+    }
+
+    public WizardDescriptor.Panel current() {
+        return getPanels()[index];
+    }
+
+    public String name() {
+        return index + 1 + ". from " + getPanels().length;
+    }
+
+    public boolean hasNext() {
+        return index < getPanels().length - 1;
+    }
+
+    public boolean hasPrevious() {
+        return index > 0;
+    }
+
+    public void nextPanel() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        index++;
+    }
+
+    public void previousPanel() {
+        if (!hasPrevious()) {
+            throw new NoSuchElementException();
+        }
+        index--;
+    }
+
+    // If nothing unusual changes in the middle of the wizard, simply:
+    public void addChangeListener(ChangeListener l) {
+    }
+
+    public void removeChangeListener(ChangeListener l) {
+    }
+    // If something changes dynamically (besides moving between panels), e.g.
+    // the number of panels changes in response to user input, then uncomment
+    // the following and call when needed: fireChangeEvent();
+    /*
+    private Set<ChangeListener> listeners = new HashSet<ChangeListener>(1); // or can use ChangeSupport in NB 6.0
+    public final void addChangeListener(ChangeListener l) {
+    synchronized (listeners) {
+    listeners.add(l);
+    }
+    }
+    public final void removeChangeListener(ChangeListener l) {
+    synchronized (listeners) {
+    listeners.remove(l);
+    }
+    }
+    protected final void fireChangeEvent() {
+    Iterator<ChangeListener> it;
+    synchronized (listeners) {
+    it = new HashSet<ChangeListener>(listeners).iterator();
+    }
+    ChangeEvent ev = new ChangeEvent(this);
+    while (it.hasNext()) {
+    it.next().stateChanged(ev);
+    }
+    }
+     */
+}
