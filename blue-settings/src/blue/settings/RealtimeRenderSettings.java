494f28f322487c2e77ac851255229ab216e83624
Steven Yi <stevenyi@gmail.com>
2011-09-19 12:23:07 +0100

    Made PortAudio blocking interface (pa_bl) the default audio driver on OSX as "PortAudio" and "pa_cb" uses the callback interface which has caused crashes

--- blue-settings/src/blue/settings/RealtimeRenderSettings.java
+++ blue-settings/src/blue/settings/RealtimeRenderSettings.java
@@ -34,110 +34,58 @@ import org.openide.util.NbPreferences;
 public class RealtimeRenderSettings implements Serializable {
 
     private static final String ADVANCED_SETTINGS = "advancedSettings";
-
     private static final String AUDIO_DRIVER = "audioDriver";
-
     private static final String AUDIO_DRIVER_ENABLED = "audioDriverEnabled";
-
     private static final String AUDIO_IN_ENABLED = "audioInEnabled";
-
     private static final String AUDIO_IN_TEXT = "audioInText";
-
     private static final String AUDIO_OUT_ENABLED = "audioOutEnabled";
-
     private static final String AUDIO_OUT_TEXT = "audioOutText";
-
     private static final String BENCHMARK_ENABLED = "benchmarkEnabled";
-
     private static final String CSOUND_EXECUTABLE = "csoundExecutable";
-
     private static final String DEFAULT_KSMPS = "defaultKsmps";
-
     private static final String DEFAULT_NCHNLS = "defaultNchnls";
-
     private static final String DEFAULT_SR = "defaultSr";
-
     private static final String HARDWARE_BUFFER_ENABLED = "hardwareBufferEnabled";
-
     private static final String HARDWARE_BUFFER_SIZE = "hardwareBufferSize";
-
     private static final String MIDI_DRIVER = "midiDriver";
-
     private static final String MIDI_DRIVER_ENABLED = "midiDriverEnabled";
-
     private static final String MIDI_IN_ENABLED = "midiInEnabled";
-
     private static final String MIDI_IN_TEXT = "midiInText";
-
     private static final String MIDI_OUT_ENABLED = "midiOutEnabled";
-
     private static final String MIDI_OUT_TEXT = "midiOutText";
-
     private static final String NOTE_AMPS_ENABLED = "noteAmpsEnabled";
-
     private static final String OUT_OF_RANGE_ENABLED = "outOfRangeEnabled";
-
     private static final String SOFTWARE_BUFFER_ENABLED = "softwareBufferEnabled";
-
     private static final String SOFTWARE_BUFFER_SIZE = "softwareBufferSize";
-
     private static final String WARNINGS_ENABLED = "warningsEnabled";
-
     private static String[] AUDIO_DRIVERS = null;
-
     private static String[] MIDI_DRIVERS = null;
-
     // PROPERTIES
     public boolean audioDriverEnabled = true;
-
     public String audioDriver = "PortAudio";
-
     public boolean midiDriverEnabled = false;
-
     public String midiDriver = "PortMidi";
-
     public boolean audioOutEnabled = true;
-
     public boolean audioInEnabled = false;
-
     public boolean midiInEnabled = false;
-
     public boolean midiOutEnabled = false;
-
     public boolean softwareBufferEnabled = false;
-
     public boolean hardwareBufferEnabled = false;
-
     public boolean noteAmpsEnabled = true;
-
     public boolean outOfRangeEnabled = true;
-
     public boolean warningsEnabled = true;
-
     public boolean benchmarkEnabled = true;
-
     public String audioOutText = "dac";
-
     public String audioInText = "adc";
-
     public String midiInText = "";
-
     public String midiOutText = "";
-
     public String csoundExecutable = "csound";
-
     public String defaultSr = "44100";
-
     public String defaultKsmps = "1";
-
     public String defaultNchnls = "2";
-
     public int softwareBufferSize = 256;
-
     public int hardwareBufferSize = 1024;
-
     public String advancedSettings = "";
-
     private static RealtimeRenderSettings instance = null;
 
     private RealtimeRenderSettings() {
@@ -151,15 +99,17 @@ public class RealtimeRenderSettings implements Serializable {
             int softwareBufferSize = 256;
             int hardwareBufferSize = 1024;
 
-			String csoundExecutableDefault = "csound";
-			
+            String csoundExecutableDefault = "csound";
+            String audioDriverDefault = "PortAudio";
+
             if (osName.indexOf("Windows") >= 0) {
                 softwareBufferSize = 4096;
                 hardwareBufferSize = 16384;
             } else if (osName.toLowerCase().indexOf("mac") >= 0) {
                 softwareBufferSize = 1024;
                 hardwareBufferSize = 4096;
-				csoundExecutableDefault = "/usr/local/bin/csound";
+                csoundExecutableDefault = "/usr/local/bin/csound";
+                audioDriverDefault = "pa_bl";
             }
 
             instance = new RealtimeRenderSettings();
@@ -174,7 +124,7 @@ public class RealtimeRenderSettings implements Serializable {
             instance.defaultNchnls = prefs.get(DEFAULT_NCHNLS, "2");
 
             instance.audioDriverEnabled = prefs.getBoolean(AUDIO_DRIVER_ENABLED, true);
-            instance.audioDriver = prefs.get(AUDIO_DRIVER, "PortAudio");
+            instance.audioDriver = prefs.get(AUDIO_DRIVER, audioDriverDefault);
             instance.audioOutEnabled = prefs.getBoolean(AUDIO_OUT_ENABLED, true);
             instance.audioOutText = prefs.get(AUDIO_OUT_TEXT, "dac");
             instance.audioInEnabled = prefs.getBoolean(AUDIO_IN_ENABLED, false);
@@ -255,8 +205,8 @@ public class RealtimeRenderSettings implements Serializable {
             boolean useMidiIn, boolean useMidiOut) {
         StringBuffer buffer = new StringBuffer();
 
-        if (APIUtilities.isCsoundAPIAvailable() &&
-                GeneralSettings.getInstance().isUsingCsoundAPI()) {
+        if (APIUtilities.isCsoundAPIAvailable()
+                && GeneralSettings.getInstance().isUsingCsoundAPI()) {
             buffer.append("csound ");
         } else {
             buffer.append(csoundExecutable).append(" ");
@@ -328,16 +278,16 @@ public class RealtimeRenderSettings implements Serializable {
 
             if (osName.indexOf("windows") >= 0) {
                 AUDIO_DRIVERS = new String[]{"PortAudio", "pa_cb", "pa_bl",
-                            "MME", "null"};
+                    "MME", "null"};
             } else if (osName.indexOf("mac") >= 0) {
                 AUDIO_DRIVERS = new String[]{"PortAudio", "pa_cb", "pa_bl",
-                            "CoreAudio", "JACK", "null"};
+                    "CoreAudio", "JACK", "null"};
             } else if (osName.indexOf("linux") >= 0) {
                 AUDIO_DRIVERS = new String[]{"PortAudio", "pa_cb", "pa_bl",
-                            "ALSA", "JACK", "pulse", "null"};
+                    "ALSA", "JACK", "pulse", "null"};
             } else {
                 AUDIO_DRIVERS = new String[]{"PortAudio", "pa_cb", "pa_bl",
-                            "MME", "ALSA", "JACK", "pulse", "CoreAudio", "null"};
+                    "MME", "ALSA", "JACK", "pulse", "CoreAudio", "null"};
             }
         }
 
