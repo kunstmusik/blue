d11c60d7dac2f39397d2a5bdeae5db5a965abc63
Steven Yi <stevenyi@gmail.com>
2011-09-29 11:41:06 +0100

    added .csoundrc Editor tool

---
+++ blue-ui-core/src/blue/ui/core/tools/OpenCsoundRCEditorAction.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blue.ui.core.tools;
+
+import blue.ui.core.tools.csoundrc.CsoundRCDialog;
+import blue.utility.GUI;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.awt.ActionRegistration;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionID;
+import org.openide.util.NbBundle.Messages;
+import org.openide.windows.WindowManager;
+
+@ActionID(category = "Application",
+id = "blue.ui.core.tools.OpenCsoundRCEditorAction")
+@ActionRegistration(displayName = "#CTL_OpenCsoundRCEditorAction")
+@ActionReferences({
+    @ActionReference(path = "Menu/Tools", position = 87)
+})
+@Messages("CTL_OpenCsoundRCEditorAction=.csoundrc Editor")
+public final class OpenCsoundRCEditorAction implements ActionListener {
+    
+    public void actionPerformed(ActionEvent e) {
+        CsoundRCDialog dialog = new CsoundRCDialog(WindowManager.getDefault().getMainWindow(), true);
+        GUI.centerOnScreen(dialog);
+        dialog.setVisible(true);
+    }
+}
