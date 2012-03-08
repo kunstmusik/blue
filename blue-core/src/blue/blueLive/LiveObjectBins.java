ecac28cb6341c5bf427081f80c8c5fc77711fdd0
Steven Yi <stevenyi@gmail.com>
2012-03-08 16:37:49 +0000

    added license

--- blue-core/src/blue/blueLive/LiveObjectBins.java
+++ blue-core/src/blue/blueLive/LiveObjectBins.java
@@ -1,6 +1,21 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * blue - object composition environment for csound
+ * Copyright (c) 2012 Steven Yi (stevenyi@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by  the Free Software Foundation; either version 2 of the License or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307 USA
  */
 package blue.blueLive;
 
@@ -275,6 +290,25 @@ public class LiveObjectBins implements Serializable {
         }
     
         return retVal;
+    }
+
+    public LiveObject getLiveObjectByUniqueId(String uniqueId) {
+        
+        if(uniqueId == null) {
+            return null;
+        }
+        
+        for(int i = 0; i < liveObjectBins.length; i++) {
+            for(int j = 0; j < liveObjectBins[0].length; j++) {
+                LiveObject lObj = liveObjectBins[i][j];
+                
+                if(lObj != null && uniqueId.equals(lObj.getUniqueId())) {
+                    return lObj;
+                }
+            }
+        }
+        
+        return null;
     }
     
 }
