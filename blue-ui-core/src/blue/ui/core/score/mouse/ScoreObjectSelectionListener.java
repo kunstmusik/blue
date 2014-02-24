f01a395638e5cb413a11ce81a750952401989a62
Steven Yi <stevenyi@gmail.com>
2014-02-24 11:38:52 -0500

    implemented double-click of score objects bringin up score object editor or editing contents of polyObject

--- blue-ui-core/src/blue/ui/core/score/mouse/ScoreObjectSelectionListener.java
+++ blue-ui-core/src/blue/ui/core/score/mouse/ScoreObjectSelectionListener.java
@@ -20,11 +20,14 @@
 package blue.ui.core.score.mouse;
 
 import blue.score.ScoreObject;
+import blue.soundObject.PolyObject;
+import blue.ui.core.score.ModeManager;
 import blue.ui.core.score.ScoreController;
+import blue.ui.core.score.ScoreTopComponent;
+import blue.ui.core.score.layers.soundObject.SoundObjectEditorTopComponent;
 import java.awt.event.MouseEvent;
 import java.util.Collection;
 import java.util.Collections;
-import org.openide.util.Utilities;
 
 /**
  *
@@ -34,13 +37,21 @@ public class ScoreObjectSelectionListener extends BlueMouseAdapter {
 
     @Override
     public void mousePressed(MouseEvent e) {
+
+        if (ModeManager.getInstance().getMode() != ModeManager.MODE_SCORE) {
+            return;
+        }
+
         if (currentScoreObjectView == null) {
             return;
         }
 
+        // consume as a click was done on a score object
+        e.consume();
+
         ScoreObject scoreObj = currentScoreObjectView.getScoreObject();
         Collection<? extends ScoreObject> selectedScoreObjects
-                = Utilities.actionsGlobalContext().lookupAll(ScoreObject.class);
+                = scoreTC.getLookup().lookupAll(ScoreObject.class);
 
         if (e.isShiftDown()) {
             if (selectedScoreObjects.contains(scoreObj)) {
@@ -48,14 +59,34 @@ public class ScoreObjectSelectionListener extends BlueMouseAdapter {
             } else {
                 ScoreController.getInstance().addSelectedScoreObject(scoreObj);
             }
-            e.consume();
         } else {
             if (!selectedScoreObjects.contains(scoreObj)) {
                 ScoreController.getInstance().setSelectedScoreObjects(
                         Collections.singleton(scoreObj));
-                e.consume();
+            } else if (e.getClickCount() == 2) {
+                editScoreObject(selectedScoreObjects, scoreObj);
             }
         }
 
+    }
+
+    protected void editScoreObject(Collection<? extends ScoreObject> selectedScoreObjects,
+            ScoreObject scoreObj) {
+        if (scoreObj instanceof PolyObject) {
+            PolyObject pObj = (PolyObject) scoreObj;
+            ScoreTopComponent.findInstance().editLayerGroup(pObj);  
+        } else {
+            if (selectedScoreObjects.size() == 1) {
+                SoundObjectEditorTopComponent editor = 
+                        SoundObjectEditorTopComponent.findInstance();
+
+                if (!editor.isOpened()) {
+                    editor.open();
+                }
+
+                editor.requestActive();
+
+            }
+        }
     }
 }
