b171bc02f616792991a370de14042663ccb6e25f
Steven Yi <stevenyi@gmail.com>
2015-04-19 14:13:49 -0400

    replaced JTextArea with Netbeans' Plain text editor (gains line numbers, find dialog) [fixes #282]

--- blue-ui-core/src/blue/gui/InfoDialog.java
+++ blue-ui-core/src/blue/gui/InfoDialog.java
@@ -17,10 +17,10 @@
  * the Free Software Foundation Inc., 59 Temple Place - Suite 330,
  * Boston, MA  02111-1307 USA
  */
-
 package blue.gui;
 
 import blue.components.CaretPositionDisplayLabel;
+import blue.ui.nbutilities.MimeTypeEditorComponent;
 import blue.ui.utilities.UiUtilities;
 import blue.utility.GUI;
 import java.awt.BorderLayout;
@@ -28,6 +28,8 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.AbstractAction;
@@ -38,70 +40,46 @@ import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 import org.openide.windows.WindowManager;
 
 /**
  * Title: blue Description: an object composition environment for csound
  * Copyright: Copyright (c) 2001 Company: steven yi music
- * 
+ *
  * @author steven yi
  * @version 1.0
  */
-
 public class InfoDialog {
-
+    
     private static JPanel infoPanel = null;
-
-    private static JTextArea infoText = null;
-
     private static JDialog dialog = null;
-
+    
     private static JTabbedPane tabs = null;
-
+    
     private static JPopupMenu popup = null;
-
-    private InfoDialog() {
-    }
-
-    private static final void initialize() {
-        infoPanel = new JPanel();
-
-        JScrollPane infoScrollPane = new JScrollPane();
-        infoText = new JTextArea();
-        infoText.setFont(new Font("Monospaced", Font.PLAIN, 12));
-
-        CaretPositionDisplayLabel caretPositionDisplayLabel = new CaretPositionDisplayLabel();
-        infoText.addCaretListener(caretPositionDisplayLabel);
-        
-        infoScrollPane.getViewport().add(infoText);
-        infoScrollPane.setPreferredSize(new Dimension(760, 400));
-
-        infoPanel.setLayout(new BorderLayout());
-        infoPanel.add(infoScrollPane, BorderLayout.CENTER);
-        infoPanel.add(caretPositionDisplayLabel, BorderLayout.SOUTH);
-    }
-
+    
+    private static MimeTypeEditorComponent infoText
+            = new MimeTypeEditorComponent("text/plain");
+    
     public static final void showInformationDialog(Component parent,
             String information, String title) {
-        if (infoPanel == null) {
-            initialize();
-        }
+        
         infoText.setText(information);
-        JOptionPane.showMessageDialog(parent, infoPanel, title,
-                JOptionPane.PLAIN_MESSAGE);
+        infoText.getJEditorPane().getCaret().setDot(0);
+        
+        final JDialog dlg = new JDialog(SwingUtilities.getWindowAncestor(parent));
+        dlg.getContentPane().add(infoText);
+        dlg.setModal(true);
+        dlg.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dlg.setTitle(title);
+        dlg.setSize(new Dimension(760, 400));
+        
+        GUI.centerOnScreen(dlg);
+        dlg.show();
         infoText.setText("");
     }
-
-//    public static final void showInformationDialogCode(Component parent,
-//            String information, String title) {
-//        BlueEditorPane bep = new BlueEditorPane();
-//
-//        bep.setText(information);
-//        bep.setCaretPosition(0);
-//        JOptionPane.showMessageDialog(parent, bep, title,
-//                JOptionPane.PLAIN_MESSAGE);
-//    }
-
+    
     public static final void showInformationDialogTabs(String information,
             String title) {
         if (dialog == null) {
@@ -110,56 +88,57 @@ public class InfoDialog {
             dialog.setTitle("Information");
             tabs = new JTabbedPane();
             dialog.getContentPane().add(tabs);
-
+            
             dialog.setSize(640, 480);
-
+            
             GUI.centerOnScreen(dialog);
-
+            
             popup = new JPopupMenu();
-
+            
             popup.add(new AbstractAction("Remove") {
-
+                
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     int index = tabs.getSelectedIndex();
                     if (index >= 0) {
                         tabs.remove(index);
-
+                        
                         if (tabs.getTabCount() == 0) {
                             dialog.setVisible(false);
                         }
                     }
                 }
-
+                
             });
-
+            
             tabs.addMouseListener(new MouseAdapter() {
-
+                
                 @Override
                 public void mousePressed(MouseEvent e) {
                     if (UiUtilities.isRightMouseButton(e)) {
                         popup.show(tabs, e.getX(), e.getY());
                     }
                 }
-
+                
             });
-
-            dialog.getRootPane().putClientProperty("SeparateWindow", Boolean.TRUE);
+            
+            dialog.getRootPane().putClientProperty("SeparateWindow",
+                    Boolean.TRUE);
         }
-
+        
         tabs.add(title, new JScrollPane(new JTextArea(information)));
         tabs.setSelectedIndex(tabs.getTabCount() - 1);
         dialog.setVisible(true);
-
+        
     }
-
+    
     public static boolean infoTabsHasTabs() {
         if (tabs == null) {
             return false;
         }
         return tabs.getTabCount() > 0;
     }
-
+    
     public static void showInfoTabsDialog() {
         if (dialog != null) {
             dialog.setVisible(!dialog.isVisible());
