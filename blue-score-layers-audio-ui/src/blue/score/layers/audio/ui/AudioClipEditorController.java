dd595261cb94e5ade2d0a27bce30b72a309ef611
Steven Yi <stevenyi@gmail.com>
2015-10-25 09:30:18 -0400

    fixed grid spacing

--- blue-score-layers-audio-ui/src/blue/score/layers/audio/ui/AudioClipEditorController.java
+++ blue-score-layers-audio-ui/src/blue/score/layers/audio/ui/AudioClipEditorController.java
@@ -25,9 +25,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
