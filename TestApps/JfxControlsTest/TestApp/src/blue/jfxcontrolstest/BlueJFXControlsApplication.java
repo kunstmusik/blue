4ea8ee3672815f7f48388e9d1f6e3a9d64ca748b
Steven Yi <stevenyi@gmail.com>
2015-10-27 15:03:27 -0700

    testing with removing scrollpane border

--- TestApps/JfxControlsTest/TestApp/src/blue/jfxcontrolstest/BlueJFXControlsApplication.java
+++ TestApps/JfxControlsTest/TestApp/src/blue/jfxcontrolstest/BlueJFXControlsApplication.java
@@ -26,8 +26,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javax.swing.BorderFactory;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
@@ -39,37 +39,37 @@ import javax.swing.table.DefaultTableModel;
  * @author stevenyi
  */
 public class BlueJFXControlsApplication extends Application {
-
+    
     @Override
     public void start(Stage stage) throws Exception {
-
+        
         TabPane root = new TabPane();
-
+        
         root.getTabs().add(new Tab("Knob", new Knob()));
-
+        
         HBox hbox = new HBox();
         hbox.setSpacing(10.0);
-
+        
         Knob knob = new Knob();
         knob.setStyle("-fx-track-fill: purple;"
                 + "-fx-track-background-fill: #333344;");
-
+        
         Knob knob2 = new Knob();
         knob2.setStyle("-fx-track-fill: green;"
                 + "-fx-track-background-fill: purple;");
         hbox.getChildren().addAll(new Knob(), knob, knob2);
-
+        
         root.getTabs().add(new Tab("Knobs", hbox));
-
+        
         ScrollPane pane = new ScrollPane();
         pane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
         pane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
         root.getTabs().add(new Tab("ScrollPane", pane));
-
+        
         setupTextFieldsTest(root);
-
+        
         setTablesTest(root);
-
+        
         Scene scene = new Scene(new BorderPane(root));
         BlueFX.style(scene);
         stage.setScene(scene);
@@ -77,7 +77,7 @@ public class BlueJFXControlsApplication extends Application {
 
         //SimpleCSSEditor.editCSS(root);
     }
-
+    
     private void setupTextFieldsTest(TabPane root) {
         SwingNode swingNode = new SwingNode();
         SwingUtilities.invokeLater(() -> swingNode.setContent(new JTextField(
@@ -90,7 +90,7 @@ public class BlueJFXControlsApplication extends Application {
         gp.setPadding(new Insets(5, 5, 5, 5));
         root.getTabs().add(new Tab("Text Fields", new BorderPane(gp)));
     }
-
+    
     private void setTablesTest(TabPane root) {
         TableView<Person> jfxTable = new TableView<>();
         ObservableList<Person> people = FXCollections.observableArrayList();
@@ -106,12 +106,12 @@ public class BlueJFXControlsApplication extends Application {
         TableColumn<Person, String> lastNameCol = new TableColumn<Person, String>(
                 "Last Name");
         lastNameCol.setCellValueFactory(new PropertyValueFactory("lastName"));
-
+        
         jfxTable.getColumns().addAll(firstNameCol, lastNameCol);
         jfxTable.setItems(people);
-
+        
         SwingNode swingNode = new SwingNode();
-
+        
         Vector v = new Vector();
         for (int i = 0; i < 20; i++) {
             Vector t = new Vector();
@@ -119,19 +119,20 @@ public class BlueJFXControlsApplication extends Application {
             t.add(Integer.toString(i));
             v.add(t);
         }
-       
+        
         Vector labels = new Vector();
         labels.add("First Name");
         labels.add("Last Name");
-        SwingUtilities.invokeLater(() -> { 
+        SwingUtilities.invokeLater(() -> {            
             DefaultTableModel tm = new DefaultTableModel(v, labels);
             JTable table = new JTable(tm);
             table.updateUI();
-//            table.setRowHeight(28);
+            JScrollPane scrollPane = new JScrollPane(table);
+            scrollPane.setBorder(BorderFactory.createEmptyBorder());
             swingNode.setContent(
-                new JScrollPane(table));
+                    scrollPane);
         });
-
+        
         root.getTabs().add(new Tab("Tables", new SplitPane(jfxTable, swingNode)));
     }
 
@@ -141,5 +142,5 @@ public class BlueJFXControlsApplication extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
+    
 }
