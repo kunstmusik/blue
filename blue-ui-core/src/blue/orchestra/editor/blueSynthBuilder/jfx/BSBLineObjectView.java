b47d8be5725c9c6244191f2f4411726cf49da9b2
Steven Yi <stevenyi@gmail.com>
2016-12-13 18:43:40 -0500

    implemented buttons enabling/disabling when number of lines changes; fixed issues when line list is empty

--- blue-ui-core/src/blue/orchestra/editor/blueSynthBuilder/jfx/BSBLineObjectView.java
+++ blue-ui-core/src/blue/orchestra/editor/blueSynthBuilder/jfx/BSBLineObjectView.java
@@ -23,6 +23,7 @@ import blue.components.lines.LineList;
 import blue.orchestra.blueSynthBuilder.BSBLineObject;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.ListChangeListener;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
@@ -43,44 +44,64 @@ public class BSBLineObjectView extends BorderPane {
         LineView lineView = new LineView(lines.getLines());
 
         lineView.selectedLineProperty().bind(selector.selectedLineProperty());
-       
+
         setCenter(lineView);
         setBottom(selector);
 
-
         setStyle("-fx-border-color:gray;");
 
+        ListChangeListener<Line> linesListener = e -> {
+            if (!lines.getLines().contains(selector.selectedLineProperty().get())) {
+                LineList newLines = lines.getLines();
+                if (newLines.size() > 0) {
+                    selector.setSelectedLine(newLines.get(0));
+                } else {
+                    selector.setSelectedLine(null);
+                }
+            }
+            selector.leftButton.setDisable(lines.getLines().size() < 2);
+            selector.rightButton.setDisable(lines.getLines().size() < 2);
+            lineView.repaint();
+        };
+
         sceneProperty().addListener((obs, old, newVal) -> {
-            if(newVal == null) {
+            if (newVal == null) {
                 lineView.widthProperty().unbind();
                 lineView.heightProperty().unbind();
                 selector.prefWidthProperty().unbind();
                 lineView.lockedProperty().unbind();
+                lines.getLines().removeListener(linesListener);
             } else {
                 lineView.widthProperty().bind(lines.canvasWidthProperty());
                 lineView.heightProperty().bind(lines.canvasHeightProperty());
                 selector.prefWidthProperty().bind(lines.canvasWidthProperty());
                 lineView.lockedProperty().bind(lines.lockedProperty());
+                lines.getLines().addListener(linesListener);
             }
         });
-    } 
 
-   
+        selector.leftButton.setDisable(lines.getLines().size() < 2);
+        selector.rightButton.setDisable(lines.getLines().size() < 2);
+    }
+
     static class LineSelector extends HBox {
+
         ObjectProperty<Line> selectedLine = new SimpleObjectProperty<>();
         private BSBLineObject lines;
-        
+
+        public Button leftButton;
+        public Button rightButton;
 
         public LineSelector(BSBLineObject lines) {
             this.lines = lines;
             Label label = new Label();
 
-            Button leftButton = new Button();
+            leftButton = new Button();
             leftButton.getStyleClass().add("left-arrow");
             leftButton.setOnAction(e -> previousLine());
             leftButton.setOnAction(e -> previousLine());
 
-            Button rightButton = new Button();
+            rightButton = new Button();
             rightButton.getStyleClass().add("right-arrow");
             rightButton.setOnAction(e -> nextLine());
 
@@ -88,25 +109,27 @@ public class BSBLineObjectView extends BorderPane {
             HBox.setHgrow(label, Priority.ALWAYS);
             getChildren().addAll(label, leftButton, rightButton);
 
-            selectedLine.addListener((obs, old, newVal) -> 
-                    label.setText(newVal.getVarName())
-            );
+            selectedLine.addListener((obs, old, newVal) -> {
+                String lblText = (newVal == null) ? ""
+                        : newVal.getVarName();
+                label.setText(lblText);
+            });
 
-            if(lines.getLines().size() > 0) {
+            if (lines.getLines().size() > 0) {
                 setSelectedLine(lines.getLines().get(0));
             }
 
         }
 
-        private void nextLine(){
+        private void nextLine() {
             Line line = getSelectedLine();
             LineList lineList = lines.getLines();
-            if(line == null || lineList.size() < 2) {
+            if (line == null || lineList.size() < 2) {
                 return;
             }
-            
+
             int index = lineList.indexOf(line) + 1;
-            if(index >= lineList.size()) {
+            if (index >= lineList.size()) {
                 index = 0;
             }
             setSelectedLine(lineList.get(index));
@@ -115,17 +138,17 @@ public class BSBLineObjectView extends BorderPane {
         private void previousLine() {
             Line line = getSelectedLine();
             LineList lineList = lines.getLines();
-            if(line == null || lineList.size() < 2) {
+            if (line == null || lineList.size() < 2) {
                 return;
             }
-            
+
             int index = lineList.indexOf(line) - 1;
-            if(index < 0) {
+            if (index < 0) {
                 index = lineList.size() - 1;
             }
             setSelectedLine(lineList.get(index));
         }
-        
+
         public void setSelectedLine(Line line) {
             selectedLine.set(line);
         }
