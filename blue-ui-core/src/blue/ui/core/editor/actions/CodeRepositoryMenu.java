637b9833c78fc2edfc07103bd633ddc0a5e16fa9
Steven Yi <stevenyi@gmail.com>
2017-04-11 01:16:49 -0400

    fixed "custom" menu option not showing menu options for code repository due to lazy=true

--- blue-ui-core/src/blue/ui/core/editor/actions/CodeRepositoryMenu.java
+++ blue-ui-core/src/blue/ui/core/editor/actions/CodeRepositoryMenu.java
@@ -47,7 +47,7 @@ import org.openide.util.actions.Presenter;
         id = "blue.ui.core.editor.actions.CodeRepositoryMenu",
 category = "Edit")
 @ActionRegistration(
-        displayName = "#codeRepositoryMenu", lazy = true)
+        displayName = "#codeRepositoryMenu")
 @ActionReferences({
     @ActionReference(
         path = "Editors/Popup", position = 1000)
