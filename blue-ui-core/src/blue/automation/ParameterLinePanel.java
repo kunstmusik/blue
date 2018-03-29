9b42144df958c1261b11e02aa2caccd9b57d982b
Steven Yi <stevenyi@gmail.com>
2018-03-29 15:10:58 -0400

    adjust line drawing to two pixel width

--- blue-ui-core/src/blue/automation/ParameterLinePanel.java
+++ blue-ui-core/src/blue/automation/ParameterLinePanel.java
@@ -32,6 +32,7 @@ import blue.ui.utilities.FileChooserManager;
 import blue.ui.utilities.UiUtilities;
 import blue.utility.NumberUtilities;
 import blue.utility.ScoreUtilities;
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
@@ -39,6 +40,7 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.Stroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -80,6 +82,8 @@ public class ParameterLinePanel extends JComponent implements
     private static final String FILE_BPF_IMPORT = "paramaterLinePanel.bpf_import";
 
     private static final String FILE_BPF_EXPORT = "paramaterLinePanel.bpf_export";
+
+    private static final Stroke STROKE2 = new BasicStroke(2);
 
     private EditPointsPopup popup = null;
 
@@ -262,6 +266,7 @@ public class ParameterLinePanel extends JComponent implements
         }
 
         Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(STROKE2);
 
         RenderingHints hints = new RenderingHints(
                 RenderingHints.KEY_ANTIALIASING,
