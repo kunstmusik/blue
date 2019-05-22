3fa5662ed96a5ad47488e2ecc4a9c9e5051d8082
Steven Yi <stevenyi@gmail.com>
2019-05-21 20:23:25 -0400

    removed unused method

--- blue-core/src/blue/mixer/Mixer.java
+++ blue-core/src/blue/mixer/Mixer.java
@@ -356,40 +356,6 @@ public class Mixer {
         return instr;
     }
 
-    public Send[] getAllSends() {
-        Send[] allSends = new Send[0];
-
-        for (int i = 0; i < this.getSubChannels().size(); i++) {
-            Channel c = this.getSubChannels().get(i);
-
-            Send[] sends = c.getSends();
-
-            if (sends.length == 0) {
-                continue;
-            }
-
-            Send[] temp = new Send[allSends.length + sends.length];
-            System.arraycopy(allSends, 0, temp, 0, allSends.length);
-            System.arraycopy(sends, 0, temp, allSends.length, sends.length);
-            allSends = temp;
-        }
-
-        for (Channel c : this.getAllSourceChannels()) {
-            Send[] sends = c.getSends();
-
-            if (sends.length == 0) {
-                continue;
-            }
-
-            Send[] temp = new Send[allSends.length + sends.length];
-            System.arraycopy(allSends, 0, temp, 0, allSends.length);
-            System.arraycopy(sends, 0, temp, allSends.length, sends.length);
-            allSends = temp;
-        }
-
-        return allSends;
-    }
-
     public boolean isSubChannel(Channel channel) {
         return subChannels.contains(channel);
     }
