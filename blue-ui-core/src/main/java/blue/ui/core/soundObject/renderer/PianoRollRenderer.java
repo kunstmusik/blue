58261b483480e1e8a8e30a5973251e5c914f37e3
Steven Yi <stevenyi@gmail.com>
2020-07-16 18:37:54 -0400

    reworked piano roll rendering to show notes on timeline bar view

--- blue-ui-core/src/main/java/blue/ui/core/soundObject/renderer/PianoRollRenderer.java
+++ blue-ui-core/src/main/java/blue/ui/core/soundObject/renderer/PianoRollRenderer.java
@@ -22,11 +22,12 @@ package blue.ui.core.soundObject.renderer;
 import blue.SoundLayer;
 import blue.plugin.BarRendererPlugin;
 import blue.soundObject.PianoRoll;
+import blue.soundObject.TimeBehavior;
 import blue.soundObject.pianoRoll.PianoNote;
 import blue.ui.core.score.layers.soundObject.SoundObjectView;
+import blue.utilities.scales.ScaleLinear;
 import java.awt.Graphics;
 import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  *
@@ -34,57 +35,187 @@ import java.util.Collections;
  */
 @BarRendererPlugin(scoreObjectType = PianoRoll.class)
 public class PianoRollRenderer extends GenericRenderer {
-    
+
     @Override
     public void render(Graphics graphics, SoundObjectView sObjView, int pixelSeconds) {
         super.render(graphics, sObjView, pixelSeconds);
-        
-        if(sObjView.getHeight() <= SoundLayer.LAYER_HEIGHT) {
+
+        PianoRoll pianoRoll = (PianoRoll) sObjView.getSoundObject();
+
+        if (sObjView.getHeight() <= SoundLayer.LAYER_HEIGHT || pianoRoll.getNotes().isEmpty()) {
             return;
         }
-        
-        PianoRoll pianoRoll = (PianoRoll)sObjView.getSoundObject();
-        
+
 //        PianoRollValueCache cache = (PianoRollValueCache) sObjView.getClientProperty(this);
 //        
 //        if(cache != null) {
 //            cac
 //        }
-        
         PianoRollValueCache cache = generateCache(pianoRoll);
-        
-        if(cache.range == 0) {
-            return;
+
+        var drawMaxHeight = sObjView.getHeight() - SoundLayer.LAYER_HEIGHT - 6;
+
+        var noteHeight = Math.min(3, Math.max(1, drawMaxHeight / cache.range));
+
+        var drawHeight = Math.min(drawMaxHeight, noteHeight * cache.range - 6);
+
+        int transY = SoundLayer.LAYER_HEIGHT + 3;
+
+        if (drawHeight < drawMaxHeight) {
+            transY += (drawMaxHeight - drawHeight) / 2;
         }
+
+        graphics.translate(0, transY);
+
+        int scaleDegrees = pianoRoll.getScale().getNumScaleDegrees();
+
+        var drawColor = (sObjView.isSelected()) ? pianoRoll.getBackgroundColor().darker() : pianoRoll.getBackgroundColor().brighter().brighter();
+        graphics.setColor(drawColor);
+
+        ScaleLinear yScale = new ScaleLinear(cache.min, cache.max, drawHeight, 0.0);
+
+        if (pianoRoll.getTimeBehavior() == TimeBehavior.SCALE) {
+            var duration = pianoRoll.getSubjectiveDuration();
+            var w = sObjView.getWidth();
+
+            ScaleLinear xScale = new ScaleLinear(0.0, cache.notesDuration, 0, w);
+
+            for (var note : pianoRoll.getNotes()) {
+                var noteNum = note.getOctave() * scaleDegrees + note.getScaleDegree();
+                var x = xScale.calc(note.getStart());
+                var y = yScale.calc(noteNum);
+
+                var width = xScale.calc(note.getDuration());
+
+                graphics.fillRect((int) x, (int) y, (int) width, noteHeight);
+            }
+
+        } else if (pianoRoll.getTimeBehavior() == TimeBehavior.REPEAT) {
+            var duration = pianoRoll.getSubjectiveDuration();
+            var repeat = pianoRoll.getRepeatPoint() > 0.0 ? pianoRoll.getRepeatPoint() : cache.notesDuration;
+
+            var curTime = 0.0;
+            var xStart = 0;
+            var windowWidth = repeat * pixelSeconds;
+
+            ScaleLinear xScale = new ScaleLinear(0.0, repeat, 0, windowWidth);
+
+            while (curTime < duration) {
+                for (var note : pianoRoll.getNotes()) {
+                    var x = xScale.calc(note.getStart());
+
+                    if (x < windowWidth) {
+                        var noteNum = note.getOctave() * scaleDegrees + note.getScaleDegree();
+                        var y = yScale.calc(noteNum);
+
+                        var width = xScale.calc(note.getDuration());
+
+                        if (x + width > windowWidth) {
+                            width = windowWidth - x;
+                        }
+
+                        graphics.fillRect((int) x + xStart, (int) y, (int) width, noteHeight);
+                    }
+                }
+                curTime += repeat;
+                xStart += windowWidth;
+            }
+
+        } else if (pianoRoll.getTimeBehavior() == TimeBehavior.REPEAT_CLASSIC) {
+            var duration = pianoRoll.getSubjectiveDuration();
+            var repeat = pianoRoll.getRepeatPoint() > 0.0 ? pianoRoll.getRepeatPoint() : cache.notesDuration;
+            var w = sObjView.getWidth();
+
+            var curTime = 0.0;
+            var xStart = 0;
+            var windowWidth = repeat * pixelSeconds;
+
+            while (curTime + repeat < duration) {
+                for (var note : pianoRoll.getNotes()) {
+                    var x = note.getStart() * pixelSeconds;
+
+                    var noteNum = note.getOctave() * scaleDegrees + note.getScaleDegree();
+                    var y = yScale.calc(noteNum);
+
+                    var width = note.getDuration() * pixelSeconds;
+
+//                    if (x + width > w) {
+//                        width = w - x;
+//                    }
+
+                    graphics.fillRect((int) x + xStart, (int) y, (int) width, noteHeight);
+
+                }
+                curTime += repeat;
+                xStart += windowWidth;
+            }
+
+            var remainingDur = duration - curTime;
+
+            for (var note : pianoRoll.getNotes()) {
+
+                if (note.getStart() + note.getDuration() < remainingDur) {
+                    var x = note.getStart() * pixelSeconds;
+                    var noteNum = note.getOctave() * scaleDegrees + note.getScaleDegree();
+                    var y = yScale.calc(noteNum);
+
+                    var width = note.getDuration() * pixelSeconds;
+
+//                    if (x + width > w) {
+//                        width = w - x;
+//                    }
+                    graphics.fillRect((int) x + xStart, (int) y, (int) width, noteHeight);
+                }
+            }
+
+        } else if (pianoRoll.getTimeBehavior() == TimeBehavior.NONE) {
+            var w = sObjView.getWidth();
+
+            for (var note : pianoRoll.getNotes()) {
+                var x = note.getStart() * pixelSeconds;
+
+                if (x < w) {
+                    var noteNum = note.getOctave() * scaleDegrees + note.getScaleDegree();
+                    var y = yScale.calc(noteNum);
+
+                    var width = note.getDuration() * pixelSeconds;
+
+                    graphics.fillRect((int) x, (int) y, (int) width, noteHeight);
+                }
+            }
+        }
+
+        graphics.translate(0, -transY);
+
     }
-    
+
     protected PianoRollValueCache generateCache(PianoRoll pianoRoll) {
         PianoRollValueCache cache = new PianoRollValueCache();
         ArrayList<PianoNote> notes = pianoRoll.getNotes();
-        
-        if(notes.size() == 0) {
-            cache.min = 0;
-            cache.max = 0;
-            cache.range = 0;
-        } else {
-            Collections.sort(notes);
-        
+
+        if (notes.size() > 0) {
             int scaleDegrees = pianoRoll.getScale().getNumScaleDegrees();
-            PianoNote noteMin = notes.get(0);
-            PianoNote noteMax = notes.get(notes.size() - 1);
-            
-            cache.min = (noteMin.getOctave() * scaleDegrees) + noteMin.getScaleDegree();
-            cache.max = (noteMax.getOctave() * scaleDegrees) + noteMax.getScaleDegree();
-            cache.range = cache.max - cache.min;
+
+            for (var note : notes) {
+                var noteNum = note.getOctave() * scaleDegrees + note.getScaleDegree();
+
+                cache.min = Math.min(cache.min, noteNum);
+                cache.max = Math.max(cache.max, noteNum);
+                cache.notesDuration = Math.max(cache.notesDuration, note.getStart() + note.getDuration());
+            }
+
+            cache.range = cache.max - cache.min + 1;
         }
-        
+
         return cache;
     }
 
     class PianoRollValueCache {
-        int max;
-        int min;
-        int range;
+
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        int range = -1;
+        double notesDuration = 0.0;
     }
-    
+
 }
