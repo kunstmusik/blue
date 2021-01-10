c364593ca3075e146525192507dc1159261b3f00
Steven Yi <stevenyi@gmail.com>
2021-01-10 01:25:30 -0500

    updated BSB fileselector view to latest changes from JFX version

--- blue-ui-core/src/main/java/blue/orchestra/editor/blueSynthBuilder/swing/BSBFileSelectorView.java
+++ blue-ui-core/src/main/java/blue/orchestra/editor/blueSynthBuilder/swing/BSBFileSelectorView.java
@@ -23,6 +23,7 @@ import blue.BlueSystem;
 import blue.orchestra.blueSynthBuilder.BSBFileSelector;
 import blue.ui.utilities.FileChooserManager;
 import blue.ui.utilities.UiUtilities;
+import blue.utility.FileUtilities;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.datatransfer.DataFlavor;
@@ -30,16 +31,20 @@ import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.dnd.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.util.List;
+import javafx.beans.value.ChangeListener;
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
-import javax.swing.JFileChooser;
+import javax.swing.JPopupMenu;
 import javax.swing.JTextField;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.util.Exceptions;
 
 /**
  * @author steven
@@ -50,49 +55,48 @@ public class BSBFileSelectorView extends BSBObjectView<BSBFileSelector> {
     private static final int FILE_BUTTON_WIDTH = 30;
     private static int OBJECT_HEIGHT = 30;
     private static String FILE_SELECTOR_ID = "BSBFileSelector";
-    
+
     JTextField fileNameField = new JTextField();
-    private BSBFileSelectorPopup popup;
+
+    ChangeListener<String> fileNameListener;
+    ChangeListener<Number> textWidthListener;
 
     public BSBFileSelectorView(BSBFileSelector selector) {
         super(selector);
-        
-        popup = new BSBFileSelectorPopup(this);
 
         fileNameField.setEditable(false);
 
         JButton fileButton = new JButton("...");
         fileButton.addActionListener((ActionEvent e) -> {
             String fileName = selector.getFileName();
-            
+
             if (fileName != null && fileName.trim().length() > 0) {
                 File file = BlueSystem.findFile(fileName);
-                
+
                 if (file != null) {
                     FileChooserManager.getDefault().setSelectedFile(
                             FILE_SELECTOR_ID,
                             file);
                 }
             }
-            
+
             List<File> rValue = FileChooserManager.getDefault().showOpenDialog(
-                    FILE_SELECTOR_ID, null);
-            
+                    FILE_SELECTOR_ID, this);
+
             if (!rValue.isEmpty()) {
                 File f = rValue.get(0);
-                
+
                 try {
                     String absFilePath = f.getCanonicalPath();
                     String relPath = BlueSystem.getRelativePath(absFilePath);
-                    
+
                     System.out.println("Rel Path: " + relPath);
                     selector.setFileName(relPath);
-                    updateDisplay();
                 } catch (IOException e1) {
                     // TODO Auto-generated catch block
                     e1.printStackTrace();
                 }
-                
+
             }
         });
 
@@ -101,6 +105,8 @@ public class BSBFileSelectorView extends BSBObjectView<BSBFileSelector> {
 
         this.setSize(selector.getTextFieldWidth() + FILE_BUTTON_WIDTH,
                 OBJECT_HEIGHT);
+
+        fileNameField.setSize(selector.getTextFieldWidth(), OBJECT_HEIGHT);
 
         this.setLayout(new BorderLayout());
         this.add(fileNameField, BorderLayout.CENTER);
@@ -112,59 +118,116 @@ public class BSBFileSelectorView extends BSBObjectView<BSBFileSelector> {
             public void mousePressed(MouseEvent e) {
                 requestFocus();
                 if (UiUtilities.isRightMouseButton(e)) {
-                    popup.show(BSBFileSelectorView.this, e.getX(), e.getY());
+                    showPopup(e.getX(), e.getY());
                 }
             }
         });
 
         new BSBFileSelectorDropTargetListener(this, fileNameField);
 
-        updateDisplay();
+        fileNameField.setText(bsbObj.getFileName());
+        fileNameField.setToolTipText(bsbObj.getFileName());
         revalidate();
+
+        fileNameListener = (obs, old, newVal) -> {
+            fileNameField.setText(newVal);
+            fileNameField.setToolTipText(bsbObj.getFileName());
+        };
+        textWidthListener = (obs, old, newVal) -> {
+            fileNameField.setSize(newVal.intValue(), OBJECT_HEIGHT);
+            setSize(getPreferredSize());
+        };
     }
 
-    
     @Override
-    public String getToolTipText() {
-        return shouldShowToolTip() ? getBSBObject().getComment() : null;
-    }
-    
-    protected void updateDisplay() {
-        fileNameField.setText(getBSBObject().getFileName());
-    }
+    public void addNotify() {
+        super.addNotify();
 
-    public int getTextFieldWidth() {
-        return getBSBObject().getTextFieldWidth();
+        bsbObj.fileNameProperty().addListener(fileNameListener);
+        bsbObj.textFieldWidthProperty().addListener(textWidthListener);
+
     }
 
-    public void setTextFieldWidth(int textFieldWidth) {
-        Dimension d = new Dimension(textFieldWidth, fileNameField.getHeight());
-
-        fileNameField.setPreferredSize(d);
-        fileNameField.setSize(d);
-
-        d = new Dimension(textFieldWidth + FILE_BUTTON_WIDTH, OBJECT_HEIGHT);
-
-        this.setPreferredSize(d);
-        this.setSize(d);
-
-        getBSBObject().setTextFieldWidth(textFieldWidth);
-
-        revalidate();
+    @Override
+    public void removeNotify() {
+        super.removeNotify();
+        bsbObj.fileNameProperty().removeListener(fileNameListener);
+        bsbObj.textFieldWidthProperty().removeListener(textWidthListener);
     }
 
     public void resetText() {
         getBSBObject().setFileName("");
-        updateDisplay();
     }
-    
-  
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(bsbObj.getTextFieldWidth() + FILE_BUTTON_WIDTH, OBJECT_HEIGHT);
+    }
+
+    private void showPopup(int x, int y) {
+        JPopupMenu popup = new JPopupMenu();
+        popup.add(new AbstractAction("Clear") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bsbObj.setFileName("");
+            }
+        });
+        popup.add(new AbstractAction("Copy to Media Folder") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                final var projectDir = BlueSystem.getCurrentProjectDirectory();
+
+                if (projectDir == null || !projectDir.exists()) {
+                    var nd = new NotifyDescriptor("Please save this project before copying to the media directory.",
+                            "Project not saved yet", NotifyDescriptor.DEFAULT_OPTION,
+                            NotifyDescriptor.ERROR_MESSAGE,
+                            null, null);
+                    DialogDisplayer.getDefault().notify(nd);
+                    return;
+                }
+
+                final var f = BlueSystem.findFile(bsbObj.getFileName());
+
+                if (f == null) {
+                    return;
+                }
+
+                final var fParent = f.getParentFile();
+
+                final var mediaFolder = BlueSystem.getCurrentBlueData().getProjectProperties().mediaFolder;
+
+                final var targetDir = FileUtilities.resolveAndCreateMediaFolder(projectDir, mediaFolder);
+
+                var targetFile = new File(targetDir, f.getName());
+
+                if (f.exists() && f.isFile() && !targetFile.equals(fParent)) {
+
+                    targetFile = FileUtilities.copyToMediaFolder(f, targetFile);
+
+                    if (targetFile != null) {
+                        String absFilePath;
+                        try {
+                            absFilePath = targetFile.getCanonicalPath();
+                            String relPath = BlueSystem.getRelativePath(absFilePath);
+                            bsbObj.setFileName(relPath);
+                        } catch (IOException ex) {
+                            Exceptions.printStackTrace(ex);
+                        }
+                    }
+
+                }
+            }
+        });
+        
+        popup.show(this, x, y);
+    }
+
     static class BSBFileSelectorDropTargetListener extends DropTargetAdapter {
 
         private final BSBFileSelectorView bsbFileSelectorView;
         private DropTarget target;
 
-        private BSBFileSelectorDropTargetListener(BSBFileSelectorView bsbFileSelectorView, 
+        private BSBFileSelectorDropTargetListener(BSBFileSelectorView bsbFileSelectorView,
                 JTextField fileNameField) {
             this.bsbFileSelectorView = bsbFileSelectorView;
             target = new DropTarget(fileNameField, this);
@@ -217,7 +280,6 @@ public class BSBFileSelectorView extends BSBObjectView<BSBFileSelector> {
                                     absFilePath);
 
                             bsbFileSelectorView.getBSBObject().setFileName(relPath);
-                            bsbFileSelectorView.updateDisplay();
                         } catch (IOException e1) {
                             // TODO Auto-generated catch block
                             e1.printStackTrace();
@@ -253,7 +315,6 @@ public class BSBFileSelectorView extends BSBObjectView<BSBFileSelector> {
                                     absFilePath);
 
                             bsbFileSelectorView.getBSBObject().setFileName(relPath);
-                            bsbFileSelectorView.updateDisplay();
                         } catch (IOException e1) {
                             // TODO Auto-generated catch block
                             e1.printStackTrace();
