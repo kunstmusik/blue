992139df5f8c0a5d36f87a7a944ff5b59f8b83ab
Steven Yi <stevenyi@gmail.com>
2021-01-16 17:45:29 -0500

    updated BSBValue editor for JFX Removal

--- blue-ui-core/src/main/java/blue/orchestra/editor/blueSynthBuilder/swing/BSBValueView.java
+++ blue-ui-core/src/main/java/blue/orchestra/editor/blueSynthBuilder/swing/BSBValueView.java
@@ -19,22 +19,21 @@
  */
 package blue.orchestra.editor.blueSynthBuilder.swing;
 
-import blue.components.lines.LineBoundaryDialog;
 import blue.orchestra.blueSynthBuilder.BSBValue;
 import blue.orchestra.editor.blueSynthBuilder.EditModeOnly;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.layout.BorderPane;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 /**
  * @author steven
  */
-public class BSBValueView extends BSBObjectView<BSBValue> implements EditModeOnly {
+public class BSBValueView extends BSBObjectView<BSBValue> implements EditModeOnly,
+        ChangeListener<String> {
 
     BorderPane pane;
 
@@ -47,94 +46,38 @@ public class BSBValueView extends BSBObjectView<BSBValue> implements EditModeOnl
         super(value);
 
         this.setLayout(new BorderLayout());
-        
+
         displayLabel = new JLabel() {
             Color bgColor = new Color(255, 255, 255, 64);
-            
+
             @Override
             protected void paintComponent(Graphics g) {
                 g.setColor(bgColor);
-                g.fillRect(0,0, getWidth(), getHeight());
-                super.paintComponent(g); 
+                g.fillRect(0, 0, getWidth(), getHeight());
+                super.paintComponent(g);
             }
-            
+
         };
         this.add(displayLabel, BorderLayout.CENTER);
 
-        updateLabel();
-        
-//        value.addPropertyChangeListener(this);
-        
         displayLabel.setOpaque(false);
     }
 
-//    public float getMinimum() {
-//        return value.getMinimum();
-//    }
-//
-//    public void setMinimum(float minimum) {
-//        if (minimum >= value.getMaximum()) {
-//            JOptionPane.showMessageDialog(null, "Error: Min value "
-//                    + "can not be set greater or equals to Max value.",
-//                    "Error", JOptionPane.ERROR_MESSAGE);
-//            return;
-//        }
-//
-//        String retVal = LineBoundaryDialog.getLinePointMethod();
-//
-//        if (retVal == null) {
-//            return;
-//        }
-//
-//        value.setMinimum(minimum, (retVal == LineBoundaryDialog.TRUNCATE));
-//    }
-//
-//    public float getMaximum() {
-//        return value.getMaximum();
-//    }
-//
-//    public void setMaximum(float maximum) {
-//        if (maximum <= value.getMinimum()) {
-//            JOptionPane.showMessageDialog(null, "Error: Max value "
-//                    + "can not be set less than or " + "equal to Min value.",
-//                    "Error", JOptionPane.ERROR_MESSAGE);
-//
-//            return;
-//        }
-//
-//        String retVal = LineBoundaryDialog.getLinePointMethod();
-//
-//        if (retVal == null) {
-//            return;
-//        }
-//        value.setMaximum(maximum, (retVal == LineBoundaryDialog.TRUNCATE));
-//    }
-//    
-//    public float getDefaultValue() {
-//        return value.getDefaultValue();
-//    }
-//    
-//    public void setDefaultValue(float defaultValue) {
-//        value.setDefaultValue(defaultValue);
-//    }
+    @Override
+    public void addNotify() {
+        super.addNotify();
 
-//    @Override
-//    public void propertyChange(PropertyChangeEvent pce) {
-//        if (pce.getSource() == this.value) {
-//            switch (pce.getPropertyName()) {
-//                case "objectName":
-//                    updateLabel();
-//                    break;
-//            }
-////            if (pce.getPropertyName().equals("updateDefaultValue")) {
-////                updating = true;
-////
-////                updateValueDisplay();
-////
-////                updating = false;
-////            }
-//        }
-//    }
+        updateLabel();
+
+        bsbObj.objectNameProperty().addListener(this);
+    }
+
+    @Override
+    public void removeNotify() {
+        super.removeNotify();
+
+        bsbObj.objectNameProperty().removeListener(this);
+    }
 
     public void updateLabel() {
         displayLabel.setText("Value: <" + bsbObj.getObjectName() + ">");
@@ -143,8 +86,8 @@ public class BSBValueView extends BSBObjectView<BSBValue> implements EditModeOnl
         revalidate();
     }
 
-//    @Override
-//    public void cleanup() {
-//        value.removePropertyChangeListener(this);
-//    }
+    @Override
+    public void changed(ObservableValue<? extends String> ov, String t, String t1) {
+        updateLabel();
+    }
 }
