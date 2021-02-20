b74cc0298d5bb3a91598139c0e3f14d7d41715eb
Steven Yi <stevenyi@gmail.com>
2021-02-19 19:16:28 -0500

    stub code for work on reimplementing time system in Blue

---
+++ blue-core/src/main/java/blue/time/Meter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blue.time;
+
+/**
+ *
+ * @author syyigmmbp
+ */
+public class Meter {
+    /** Minimum 1, max unbounded */
+    long numBeats;
+    
+    /** Duration of beat, should be 1, 2, 4, 8, 16, 32 */
+    long beatLength;  
+}
---
+++ blue-core/src/main/java/blue/time/TimeUtilities.java
@@ -0,0 +1,33 @@
+package blue.time;
+
+/*
+ * blue - object composition environment for csound
+ * Copyright (c) 2020 Steven Yi (stevenyi@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by  the Free Software Foundation; either version 2 of the License or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307 USA
+ */
+
+/**
+ * 
+ * Utility class for converting time values according to meter and tempo. 
+ * Support conversions from Csound beat time, samples, bar/beats, 
+ * Min:Sec:Ms, H:M:S.F.
+ *
+ * @author Steven Yi
+ */
+public class TimeUtilities {
+    
+}
