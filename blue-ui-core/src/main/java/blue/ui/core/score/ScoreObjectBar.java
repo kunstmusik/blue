941773e6a4a344edc6134de5f208968935accdb1
Steven Yi <stevenyi@gmail.com>
2023-07-21 11:18:38 -0400

    revalidate layout after polyObject selection [fixes #749]

--- blue-ui-core/src/main/java/blue/ui/core/score/ScoreObjectBar.java
+++ blue-ui-core/src/main/java/blue/ui/core/score/ScoreObjectBar.java
@@ -239,6 +239,7 @@ public final class ScoreObjectBar extends JComponent implements ActionListener,
             }
         }
         currentPath = path;
+        revalidate();
         repaint();
     }
 
